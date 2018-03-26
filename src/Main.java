a7e1b94c1e51ab7c6b56498b75901e00fe143ea1
sheesania <sheesania@users.noreply.github.com>
2018-03-26 16:17:45 -0600

    Connection to postgres using configuration stored in properties file

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.io.FileInputStream;
+import java.sql.*;
+import java.util.Properties;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            Properties props = new Properties();
+            FileInputStream in = new FileInputStream("config.properties");
+            props.load(in);
+            in.close();
+            Connection connection = DriverManager.getConnection(props.getProperty("url"), props);
+            System.out.println(connection.getMetaData().getDatabaseProductName());
+        } catch (Exception e) {
+            System.out.println("Error establishing database connection");
+            System.out.println(e.getMessage());
+        }
     }
 }
