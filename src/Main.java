5b11b1c1a2c8ed8c317d60106d35643b39048626
Andrew Blomenberg <andrewBlomen@gmail.com>
2018-03-26 17:12:49 -0600

    First draft of json parsing code. Tested and working with one dataset

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import com.google.gson.Gson;
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Gson jsonParser = new Gson();
+        String jsonPath = "testData/TestingGeojson.json";
+        Scanner inStream = null;
+        try {
+            inStream = new Scanner(new File(jsonPath));
+        }
+        catch (FileNotFoundException err) { }
+
+        String json = inStream.useDelimiter("\\Z").next();
+
+        GeoJsonFile geoJsonFile = jsonParser.fromJson(json, GeoJsonFile.class);
+
+        System.out.println(geoJsonFile.features.size());
     }
 }
