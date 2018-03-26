13d1d705800343ea90e488677cf7914c8325d977
sheesania <sheesania@users.noreply.github.com>
2018-03-26 17:15:03 -0600

    CSV and tab delimited input working

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.sql.*;
 import java.util.Properties;
+import org.apache.commons.csv.*;
 
 public class Main {
 
@@ -14,6 +16,28 @@ public class Main {
             System.out.println(connection.getMetaData().getDatabaseProductName());
         } catch (Exception e) {
             System.out.println("Error establishing database connection");
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            FileReader in = new FileReader("testfiles/TestingUSBGN.csv");
+            Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(in);
+            for (CSVRecord record : records) {
+                String name = record.get("FEATURE_NAME");
+            }
+        } catch (Exception e) {
+            System.out.println("error processing csv");
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            FileReader in = new FileReader("testfiles/TestingCountries_populatedplaces_p.txt");
+            Iterable<CSVRecord> records = CSVFormat.TDF.withFirstRecordAsHeader().parse(in);
+            for (CSVRecord record : records) {
+                String name = record.get("FULL_NAME_RO");
+            }
+        } catch (Exception e) {
+            System.out.println("error processing txt");
             System.out.println(e.getMessage());
         }
     }
