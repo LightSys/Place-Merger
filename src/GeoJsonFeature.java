41083c39fd230a1a93faa09242256e0df77fbd12
Andrew Blomenberg <andrewBlomen@gmail.com>
2018-03-27 11:47:55 -0600

    Tweaks to the Geojson classes

--- src/GeoJsonFeature.java
+++ src/GeoJsonFeature.java
@@ -3,10 +3,8 @@ import java.util.Collection;
 public class GeoJsonFeature {
     public String type;
     class Properties{
-        public int OBJECTID;
         public int osm_id;
-        public short code;
-        public String fclass;
+        public short code; //feature code
         public int population;
         public String name;
         public double Shape_Length;
