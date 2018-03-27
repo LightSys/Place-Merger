5dd58f559dfcf35f855e1ff70e677eef160c5ae9
Andrew Blomenberg <andrewBlomen@gmail.com>
2018-03-27 15:29:37 -0600

    GeojsonToUDb mostly complete

---
+++ src/GeojsonToUDb.java
@@ -0,0 +1,109 @@
+import com.google.gson.Gson;
+import java.io.*;
+import java.sql.*;
+import java.util.Collection;
+import java.util.Scanner;
+
+public class GeojsonToUDb extends SourceToUdb {
+    public static void main(String[] args) {
+        new GeojsonToUDb().run(args);
+    }
+    protected void loadIntoUDb(Connection connection, File file) {
+        Gson jsonParser = new Gson();
+        Scanner inStream = null;
+        try { inStream = new Scanner(file);}
+        catch (IOException err) {
+            System.out.println(err.getMessage());
+            return;
+        }
+
+        String json = inStream.useDelimiter("\\Z").next();
+        GeoJsonFile geoJsonFile = jsonParser.fromJson(json, GeoJsonFile.class);
+
+        System.out.println(geoJsonFile.features.size());
+
+        try {
+            insertIntoUDb(connection, geoJsonFile);
+        }
+        catch (SQLException err) {
+            System.out.println("Error inserting into UDb");
+            System.out.println(err.getMessage());
+            return;
+        }
+
+    }
+    public static void insertIntoUDb(Connection connection, GeoJsonFile geoJsonFile) throws SQLException
+    {
+        PreparedStatement all_placesExist = connection.prepareStatement("SELECT FROM all_places WHERE osm_id = ?");
+        PreparedStatement all_placesUpdateName = connection.prepareStatement("UPDATE all_places SET ");
+        PreparedStatement all_placesInsert = connection.prepareStatement(
+                "INSERT INTO all_places (primary_name, osm_id, feature_code, population) VALUES (?, ?, ?, ?)");
+        PreparedStatement polygonsInsert = connection.prepareStatement(
+                "INSERT INTO polygons (coordinates, shape_length, shape_area) VALUES (?, ?, ?)");
+
+        for(GeoJsonFeature feature: geoJsonFile.features) {
+            if (feature.properties.osm_id == 0)
+                throw new SQLException("There is a feature with no osm_id");
+
+            all_placesExist.setInt(1,feature.properties.osm_id);
+            ResultSet resultSet = all_placesExist.executeQuery();
+            if(resultSet.next()) {//if there is already a record with this osm_id
+
+            } else {
+                all_placesInsert.setString(1,feature.properties.name);
+                all_placesInsert.setInt(2,feature.properties.osm_id);
+                all_placesInsert.setInt(3,feature.properties.code);
+                all_placesInsert.setInt(4,feature.properties.population);
+                all_placesInsert.executeUpdate();
+            }
+
+            //insert into polygons table
+            if(! feature.geometry.type.equals("MultiPolygon"))
+                throw new SQLException("One of the polygons was not a MultiPolygon");
+
+            Double[][] coords = convertCollsToArr(feature.geometry.coordinates);
+
+            polygonsInsert.setArray(1,connection.createArrayOf("DOUBLE PRECISION", coords));
+            polygonsInsert.setDouble(2, feature.properties.Shape_Length);
+            polygonsInsert.setDouble(3, feature.properties.Shape_Area);
+
+            polygonsInsert.executeUpdate();
+        }
+
+        all_placesExist.close();
+        all_placesUpdate.close();
+        all_placesInsert.close();
+        polygonsInsert.close();
+    }
+
+    public static Double[][] convertCollsToArr(Collection<Collection<Collection<Collection<Double>>>> coordColl)
+    {
+        Collection<Collection<Double>> trimmed = coordColl.iterator().next().iterator().next();
+
+        int numCols = 0;
+        int numRows = 0;
+
+        for(Collection<Double> coll: trimmed){
+            numRows++;
+            for(Double doub: coll) {
+                numCols++;
+            }
+        }
+
+        Double[][] outArr = new Double[numRows][numCols];
+
+        int rowIdx = 0;
+        int colIdx = 0;
+
+        for(Collection<Double> coll: trimmed){
+            for(Double doub: coll) {
+                outArr[rowIdx][colIdx] = doub;
+                colIdx++;
+            }
+            colIdx = 0;
+            rowIdx++;
+        }
+
+        return outArr;
+    }
+}
